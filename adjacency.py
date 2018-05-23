002fcd5b89eebe00c5d6e5fc8d11b6af76e1839b
dominik <dominikheinisch2@gmail.com>
2018-05-23 21:01:09 +0200

    added consistency pairs

--- adjacency.py
+++ adjacency.py
@@ -17,18 +17,20 @@ def find_pairs_euclidean(l1, l2):
 def find_nearest_neighbors(points, n):
     v = np.array([x.coords for x in points])
     distances = distance.cdist(v, v, metric='euclidean')
-    l = []
+    list_ = []
     for i in range(len(points)):
         closest = points[np.argsort(distances[i])[1:(n + 1)]]
-        l.append((points[i], closest))
-        print(closest)
-    return l
+        list_.append((points[i], closest))
+        # print(closest)
+    return list_
 
 
 def filter_points(pairs, n, threshold):
+    # print(pairs)
     points1 = np.array([x[0] for x in pairs])
     points2 = np.array([x[1] for x in pairs])
     l1 = find_nearest_neighbors(points1, n)
+    # print(l1)
     l2 = find_nearest_neighbors(points2, n)
     filtered_pairs = []
     for i in range(len(points1)):
@@ -36,3 +38,53 @@ def filter_points(pairs, n, threshold):
             if(l1[i][1][j].id == pairs[i][1]):
                 pass
 
+
+
+
+
+
+def filter_pairs(pairs, n, threshold):
+    points1 = np.array([x[0] for x in pairs])
+    points2 = np.array([x[1] for x in pairs])
+    dist1 = generate_dist(points1)
+    dist2 = generate_dist(points2)
+    neightbours1 = get_n_smallest(dist1, n)
+    neightbours2 = get_n_smallest(dist2, n)
+    consistancy = get_equivalens_size(neightbours1, neightbours2)
+    indexes = selection(consistancy, threshold)
+    # print(indexes)
+    return np.asarray(pairs)[indexes]
+    # print(pairs)
+
+    # print(dist1.shape)
+    # print(neightbours1)
+    # print(neightbours2)
+    # print(consistancy)
+
+
+def generate_dist(points):
+    v = np.array([p.coords for p in points])
+    return distance.cdist(v, v, metric='euclidean')
+
+
+def get_n_smallest(dist, n):
+    temp = np.argsort(dist, axis=1)[:, 0: n + 1]
+    col_size = dist.shape[0]
+    result = temp[:, 0:n]
+    for i in range(col_size):
+        for j in range(n):
+            if result[i, j] == i:
+                result[i, j] = temp[i, n]
+    return result
+
+
+def get_equivalens_size(neightbours1, neightbours2):
+    col_size = neightbours1.shape[0]
+    result = np.zeros(col_size)
+    for i in range(col_size):
+        result[i] = np.intersect1d(neightbours1[i], neightbours2[i]).shape[0]
+    result /= neightbours1.shape[1]
+    return result
+
+def selection(consistancy, threshold):
+    return np.argwhere(consistancy > threshold).flatten()
--- main.py
+++ main.py
@@ -7,15 +7,16 @@ IMAGES_2 = ['data/2/DSC_5824.png', 'data/2/DSC_5825.png']
 IMAGES_3 = ['data/3/3-1.png', 'data/3/3-2.png']
 
 
-CURRENT = IMAGES_1
+CURRENT = IMAGES_2
 
 
 def main():
     s1, s2 = loader.load_sifts(CURRENT)
     pairs = adjacency.find_pairs_euclidean(s1, s2)
-    adjacency.ff(pairs, 5)
-    printer.print_image(CURRENT, pairs)
-
+    # adjacency.filter_points(pairs, 5, 0.79)
+    # printer.print_image(CURRENT, pairs)
+    filtered = adjacency.filter_pairs(pairs, 5, 0.59)
+    printer.print_image(CURRENT, filtered)
 
 if __name__ == "__main__":
     main()
