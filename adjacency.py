52f29f20af79ff45a4587b159a2e9010e51df1a2
Kacper <kcpr51@hotmail.com>
2018-05-23 21:14:58 +0200

    Refactor

--- adjacency.py
+++ adjacency.py
@@ -14,61 +14,25 @@ def find_pairs_euclidean(l1, l2):
     return pairs
 
 
-def find_nearest_neighbors(points, n):
-    v = np.array([x.coords for x in points])
-    distances = distance.cdist(v, v, metric='euclidean')
-    list_ = []
-    for i in range(len(points)):
-        closest = points[np.argsort(distances[i])[1:(n + 1)]]
-        list_.append((points[i], closest))
-        # print(closest)
-    return list_
-
-
-def filter_points(pairs, n, threshold):
-    # print(pairs)
-    points1 = np.array([x[0] for x in pairs])
-    points2 = np.array([x[1] for x in pairs])
-    l1 = find_nearest_neighbors(points1, n)
-    # print(l1)
-    l2 = find_nearest_neighbors(points2, n)
-    filtered_pairs = []
-    for i in range(len(points1)):
-        for j in range(n):
-            if(l1[i][1][j].id == pairs[i][1]):
-                pass
-
-
-
-
-
-
 def filter_pairs(pairs, n, threshold):
     points1 = np.array([x[0] for x in pairs])
     points2 = np.array([x[1] for x in pairs])
-    dist1 = generate_dist(points1)
-    dist2 = generate_dist(points2)
-    neightbours1 = get_n_smallest(dist1, n)
-    neightbours2 = get_n_smallest(dist2, n)
-    consistancy = get_equivalens_size(neightbours1, neightbours2)
-    indexes = selection(consistancy, threshold)
-    # print(indexes)
+    dist1 = calc_dist(points1)
+    dist2 = calc_dist(points2)
+    neighbors1 = get_n_smallest(dist1, n)
+    neighbors2 = get_n_smallest(dist2, n)
+    consistency = get_equivalent_size(neighbors1, neighbors2)
+    indexes = selection(consistency, threshold)
     return np.asarray(pairs)[indexes]
-    # print(pairs)
-
-    # print(dist1.shape)
-    # print(neightbours1)
-    # print(neightbours2)
-    # print(consistancy)
 
 
-def generate_dist(points):
+def calc_dist(points):
     v = np.array([p.coords for p in points])
     return distance.cdist(v, v, metric='euclidean')
 
 
 def get_n_smallest(dist, n):
-    temp = np.argsort(dist, axis=1)[:, 0: n + 1]
+    temp = np.argsort(dist, axis=1)[:, 0:n + 1]
     col_size = dist.shape[0]
     result = temp[:, 0:n]
     for i in range(col_size):
@@ -78,13 +42,14 @@ def get_n_smallest(dist, n):
     return result
 
 
-def get_equivalens_size(neightbours1, neightbours2):
-    col_size = neightbours1.shape[0]
+def get_equivalent_size(neighbors1, neighbors2):
+    col_size = neighbors1.shape[0]
     result = np.zeros(col_size)
     for i in range(col_size):
-        result[i] = np.intersect1d(neightbours1[i], neightbours2[i]).shape[0]
-    result /= neightbours1.shape[1]
+        result[i] = np.intersect1d(neighbors1[i], neighbors2[i]).shape[0]
+    result /= neighbors1.shape[1]
     return result
 
-def selection(consistancy, threshold):
-    return np.argwhere(consistancy > threshold).flatten()
+
+def selection(consistency, threshold):
+    return np.argwhere(consistency > threshold).flatten()
--- main.py
+++ main.py
@@ -7,15 +7,13 @@ IMAGES_2 = ['data/2/DSC_5824.png', 'data/2/DSC_5825.png']
 IMAGES_3 = ['data/3/3-1.png', 'data/3/3-2.png']
 
 
-CURRENT = IMAGES_2
+CURRENT = IMAGES_1
 
 
 def main():
     s1, s2 = loader.load_sifts(CURRENT)
     pairs = adjacency.find_pairs_euclidean(s1, s2)
-    # adjacency.filter_points(pairs, 5, 0.79)
-    # printer.print_image(CURRENT, pairs)
-    filtered = adjacency.filter_pairs(pairs, 5, 0.59)
+    filtered = adjacency.filter_pairs(pairs, 5, 0.79)
     printer.print_image(CURRENT, filtered)
 
 if __name__ == "__main__":
