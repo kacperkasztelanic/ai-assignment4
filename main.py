0feb57eaeb3a254133d77506257811a76c141941
dominik <dominikheinisch2@gmail.com>
2018-06-05 03:14:24 +0200

    added heuristics, oop code

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ IMAGES_1 = ['data/1/DSC03230.png', 'data/1/DSC03240.png']
 IMAGES_2 = ['data/2/DSC_5824.png', 'data/2/DSC_5825.png']
 IMAGES_3 = ['data/3/3-1.png', 'data/3/3-2.png']
 
-CURRENT = IMAGES_1
+PATHS = IMAGES_1
 
 
 @timing
 def main():
-    key_points_1, key_points_2 = loader.load_sifts(CURRENT)
+    key_points_1, key_points_2 = loader.load_sifts(PATHS)
     print(key_points_1)
     pairs = adjacency.find_pairs_euclidean(key_points_1, key_points_2)
 
     n = 25
     t = 0.81
     filtered_pairs = adjacency.filter_pairs(pairs, n=n, threshold=t)
-    printer.print_image(CURRENT, filtered_pairs, 'adjacency_n{}_t{}.png'.format(n, t))
+    printer.print_image(PATHS, filtered_pairs, 'adjacency_n{}_t{}.png'.format(n, t))
 
     i = 500
-    s = 3
+    size = 3
     e = 1
-    model = ransac.ransac_model(filtered_pairs, s=s, iter=i, max_error=e)
-    ransac_pairs = ransac.ransac_pairs(filtered_pairs, model)
-    printer.print_image(CURRENT, ransac_pairs, 'ransac_n{}_t{}_s{}_i{}_e{}.png'.format(n, t, s, i, e))
-    printer.print_all_image(CURRENT, key_points_1, key_points_2, filtered_pairs, ransac_pairs,
+    ransac_ = ransac.Ransac(PATHS, filtered_pairs, heuristic=ransac.DistanceHeuristic.distance_heuristic)
+    ransac_.calculate(size=size, no_draws=i, max_error=e)
+    ransac_pairs = ransac_.get_ransac_pairs()
+
+    s = size
+    printer.print_image(PATHS, ransac_pairs, 'ransac_n{}_t{}_s{}_i{}_e{}.png'.format(n, t, s, i, e))
+    printer.print_all_image(PATHS, key_points_1, key_points_2, filtered_pairs, ransac_pairs,
                             'all_ransac_n{}_t{}_s{}_i{}_e{}.png'.format(n, t, s, i, e))
 
 
--- ransac.py
+++ ransac.py
@@ -1,97 +1,138 @@
+from enum import Enum
+from PIL import Image
 import numpy as np
 from scipy.spatial import distance
 
 from Point import Point
 
 
-def is_invertible(a):
-    return a.shape[0] == a.shape[1] and np.linalg.matrix_rank(a) == a.shape[0]
+class DistanceHeuristic(Enum):
+    default_none = 0
+    distance_heuristic = 1
 
 
-def affine_array(x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3):
-    a1 = np.array([[x1, y1, 1, 0, 0, 0],
-                   [x2, y2, 1, 0, 0, 0],
-                   [x3, y3, 1, 0, 0, 0],
-                   [0, 0, 0, x1, y1, 1],
-                   [0, 0, 0, x2, y2, 1],
-                   [0, 0, 0, x3, y3, 1]])
-    a2 = np.array([u1, u2, u3, v1, v2, v3])
-    if not is_invertible(a1):
-        return None
-    a = np.linalg.inv(a1) @ a2
-    res = np.reshape(np.append(a, [0, 0, 1]), newshape=(3, 3))
-    return res
+class Ransac:
 
+    def __init__(self, paths, filtered_pairs, heuristic=DistanceHeuristic.default_none, lower_limit=0.01,
+                 upper_limit=0.5):
+        self.heuristic = heuristic
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+        self.filtered_pairs = filtered_pairs
+        self.init_limits(paths)
+        # self.matrix_A = None
+        # self.matrix_H = None
+        self.model = None
+        self._ransac_pairs = []
 
-def perspective_array(x1, y1, x2, y2, x3, y3, x4, y4, u1, v1, u2, v2, u3, v3, u4, v4):
-    a1 = np.array([[x1, y1, 1, 0, 0, 0, -u1 * x1, -u1 * y1],
-                   [x2, y2, 1, 0, 0, 0, -u2 * x2, -u2 * y2],
-                   [x3, y3, 1, 0, 0, 0, -u3 * x3, -u3 * y3],
-                   [x4, y4, 1, 0, 0, 0, -u4 * x4, -u4 * y4],
-                   [0, 0, 0, x1, y1, 1, -v1 * x1, -v1 * y1],
-                   [0, 0, 0, x2, y2, 1, -v2 * x2, -v2 * y2],
-                   [0, 0, 0, x3, y3, 1, -v3 * x3, -v3 * y3],
-                   [0, 0, 0, x4, y4, 1, -v4 * x4, -v4 * y4]])
-    a2 = np.array([u1, u2, u3, u4, v1, v2, v3, v4])
-    if not is_invertible(a1):
-        return None
-    a = np.linalg.inv(a1) @ a2
-    res = np.reshape(np.append(a, 1), newshape=(3, 3))
-    return res
+    def init_limits(self, paths):
+        img_1 = Image.open(paths[0])
+        img_2 = Image.open(paths[1])
+        size = max(sum(img_1.size), sum(img_2.size)) / 2
+        self.lower_limit *= size
+        self.upper_limit *= size
 
+    def calculate(self, size, no_draws, max_error):
+        self.ransac_model(size, no_draws, max_error)
+        self.calculate_ransac_pairs()
 
-def calc_model(samples):
-    x1 = samples[0][0].coords[0]
-    y1 = samples[0][0].coords[1]
-    x2 = samples[1][0].coords[0]
-    y2 = samples[1][0].coords[1]
-    x3 = samples[2][0].coords[0]
-    y3 = samples[2][0].coords[1]
-    u1 = samples[0][1].coords[0]
-    v1 = samples[0][1].coords[1]
-    u2 = samples[1][1].coords[0]
-    v2 = samples[1][1].coords[1]
-    u3 = samples[2][1].coords[0]
-    v3 = samples[2][1].coords[1]
-    if len(samples) == 3:
-        a = affine_array(x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3)
-    else:
-        x4 = samples[3][0].coords[0]
-        y4 = samples[3][0].coords[1]
-        u4 = samples[3][1].coords[0]
-        v4 = samples[3][1].coords[1]
-        a = perspective_array(x1, y1, x2, y2, x3, y3, x4, y4, u1, v1, u2, v2, u3, v3, u4, v4)
-    return a
+    def ransac_model(self, size, no_draws, max_error):
+        pairs = self.filtered_pairs
+        best_model = None
+        best_score = 0
+        for i in range(no_draws):
+            model = None
+            while model is None:
+                indices = np.random.choice(pairs.shape[0], size=size)
+                chosen = pairs[indices]
+                if self.heuristic == DistanceHeuristic.default_none:
+                    model = self.calc_model(chosen)
+                elif self.heuristic == DistanceHeuristic.distance_heuristic:
+                    v = np.array([p[0].coords for p in chosen])
+                    dist = distance.cdist(v, v, metric='euclidean')
+                    np.fill_diagonal(dist, self.upper_limit)
+                    if np.min(dist) < self.lower_limit or np.min(dist) > self.upper_limit:
+                        model = None
+                    else:
+                        model = self.calc_model(chosen)
+            score = 0
+            for pair in pairs:
+                error = self.model_error(model, pair)
+                if error < max_error:
+                    score += 1
+            if score > best_score:
+                best_score = score
+                best_model = model
+        self.model = best_model
 
+    def calc_model(self, samples):
+        x1 = samples[0][0].coords[0]
+        y1 = samples[0][0].coords[1]
+        x2 = samples[1][0].coords[0]
+        y2 = samples[1][0].coords[1]
+        x3 = samples[2][0].coords[0]
+        y3 = samples[2][0].coords[1]
+        u1 = samples[0][1].coords[0]
+        v1 = samples[0][1].coords[1]
+        u2 = samples[1][1].coords[0]
+        v2 = samples[1][1].coords[1]
+        u3 = samples[2][1].coords[0]
+        v3 = samples[2][1].coords[1]
+        if len(samples) == 3:
+            a = self.affine_array(x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3)
+        else:
+            x4 = samples[3][0].coords[0]
+            y4 = samples[3][0].coords[1]
+            u4 = samples[3][1].coords[0]
+            v4 = samples[3][1].coords[1]
+            a = self.perspective_array(x1, y1, x2, y2, x3, y3, x4, y4, u1, v1, u2, v2, u3, v3, u4, v4)
+        return a
 
-def model_error(model, pair):
-    return distance.cdist(np.reshape(model @ np.array([pair[0].coords[0], pair[0].coords[1], 1]), newshape=(-1, 1)),
-                          np.array([pair[1].coords[0], pair[1].coords[1], 1]).reshape(-1, 1), metric='euclidean').flatten()[0]
+    def affine_array(self, x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3):
+        a1 = np.array([[x1, y1, 1, 0, 0, 0],
+                       [x2, y2, 1, 0, 0, 0],
+                       [x3, y3, 1, 0, 0, 0],
+                       [0, 0, 0, x1, y1, 1],
+                       [0, 0, 0, x2, y2, 1],
+                       [0, 0, 0, x3, y3, 1]])
+        a2 = np.array([u1, u2, u3, v1, v2, v3])
+        if not self.is_invertible(a1):
+            return None
+        a = np.linalg.inv(a1) @ a2
+        res = np.reshape(np.append(a, [0, 0, 1]), newshape=(3, 3))
+        return res
 
+    @staticmethod
+    def is_invertible(a):
+        return a.shape[0] == a.shape[1] and np.linalg.matrix_rank(a) == a.shape[0]
 
-def ransac_model(pairs, s, iter, max_error):
-    best_model = None
-    best_score = 0
-    for i in range(iter):
-        model = None
-        while model is None:
-            indices = np.random.choice(pairs.shape[0], size=s)
-            chosen = pairs[indices]
-            model = calc_model(chosen)
-        score = 0
-        for pair in pairs:
-            error = model_error(model, pair)
-            if (error < max_error):
-                score += 1
-        if score > best_score:
-            best_score = score
-            best_model = model
-    return best_model
+    def perspective_array(self, x1, y1, x2, y2, x3, y3, x4, y4, u1, v1, u2, v2, u3, v3, u4, v4):
+        a1 = np.array([[x1, y1, 1, 0, 0, 0, -u1 * x1, -u1 * y1],
+                       [x2, y2, 1, 0, 0, 0, -u2 * x2, -u2 * y2],
+                       [x3, y3, 1, 0, 0, 0, -u3 * x3, -u3 * y3],
+                       [x4, y4, 1, 0, 0, 0, -u4 * x4, -u4 * y4],
+                       [0, 0, 0, x1, y1, 1, -v1 * x1, -v1 * y1],
+                       [0, 0, 0, x2, y2, 1, -v2 * x2, -v2 * y2],
+                       [0, 0, 0, x3, y3, 1, -v3 * x3, -v3 * y3],
+                       [0, 0, 0, x4, y4, 1, -v4 * x4, -v4 * y4]])
+        a2 = np.array([u1, u2, u3, u4, v1, v2, v3, v4])
+        if not self.is_invertible(a1):
+            return None
+        a = np.linalg.inv(a1) @ a2
+        res = np.reshape(np.append(a, 1), newshape=(3, 3))
+        return res
 
+    @staticmethod
+    def model_error(model, pair):
+        return distance.cdist(np.reshape(model @ np.array([pair[0].coords[0], pair[0].coords[1], 1]), newshape=(-1, 1)),
+                              np.array([pair[1].coords[0], pair[1].coords[1], 1]).reshape(-1, 1),
+                              metric='euclidean').flatten()[0]
 
-def ransac_pairs(pairs, model):
-    transformed_pairs = []
-    for pair in pairs:
-        temp = model @ np.array([pair[0].coords[0], pair[0].coords[1], 1])
-        transformed_pairs.append((pair[0], Point((temp[0], temp[1]))))
-    return transformed_pairs
+    def calculate_ransac_pairs(self):
+        self._ransac_pairs = []
+        for pair in self.filtered_pairs:
+            temp = self.model @ np.array([pair[0].coords[0], pair[0].coords[1], 1])
+            self._ransac_pairs.append((pair[0], Point((temp[0], temp[1]))))
+
+    def get_ransac_pairs(self):
+        return self._ransac_pairs
