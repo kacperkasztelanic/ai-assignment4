b898fa7286abc8ebc383db6a143591960646cc10
Kacper <kcpr51@hotmail.com>
2018-06-10 14:29:44 +0200

    Delete comments

--- adjacency.py
+++ adjacency.py
@@ -4,13 +4,6 @@ import sys
 
 
 def corresponding_points(l1, l2):
-    """
-    For each pair of FeaturedPoint finds euclidean distance of theirs vector, and returns pairs of points that are
-    closest to each others
-    :param l1: points from image 1
-    :param l2: points from image 2
-    :return:
-    """
     distances = distance.cdist(
         np.array([point.vector for point in l1]),
         np.array([point.vector for point in l2]), metric='euclidean')
@@ -23,13 +16,6 @@ def corresponding_points(l1, l2):
 
 
 def filter_pairs(pairs, n, threshold):
-    """
-    Filter pairs based on n and threshold
-    :param pairs: vector of pairs
-    :param n: Quantity of closest points
-    :param threshold: value in range 0.0 - 1.0
-    :return: filtered vector of pairs
-    """
     neighbors1 = n_closest_points(pairs[:, 0], n)
     neighbors2 = n_closest_points(pairs[:, 1], n)
     consistency = corresponding_coverage(neighbors1, neighbors2)
@@ -37,25 +23,12 @@ def filter_pairs(pairs, n, threshold):
 
 
 def n_closest_points(points, n):
-    """
-    For set of a points finds Euclidean distance on Cartesian coordinate system for each permutation of points
-    Based on distance matrix finds n closest points for each points
-    :param points: set of Points 1xN
-    :param n: Quantity of closest points
-    :return: MxN
-    """
     v = np.array([p.coords for p in points])
     dist = distance.cdist(v, v, metric='euclidean')
     return np.argsort(dist + np.eye(dist.shape[0]) * sys.maxsize, axis=1)[:, :n]
 
 
 def corresponding_coverage(neighbors1, neighbors2):
-    """
-    For corresponding points finds percentage how many closest points are matching on both images
-    :param neighbors1: matrix MxN
-    :param neighbors2: matrix MxN
-    :return: vector M contains values in range 0.0 - 1.0
-    """
     col_size = neighbors1.shape[0]
     result = np.zeros(col_size)
     for i in range(col_size):
